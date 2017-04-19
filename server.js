1468246dc9dc78e3af895c750c6e8ec12fff9a75
maliklau <malikl0799@gmail.com>
2017-04-19 14:15:19 -0400

    math random doesn't seem to work??

--- server.js
+++ server.js
@@ -1,14 +1,40 @@
 // Require Node's http module and assign it to a variable
 var http = require('http')
+var randInt
+var randInt2
 
-// Create a new server that just says "Hi!!" at every route
 var server = http.createServer(function (request, response) {
-  response.end('Hello, there!!')
+  if (request.url === '/') {
+    response.end(
+      '<h1>Welcome!!</h1>'
+    )
+  } else if (request.url === '/cuteness') {
+    response.end(
+      '<h1>Cute Animal!!</h1>' +
+      '<img src="http://static.boredpanda.com/blog/wp-content/uuuploads/cute-baby-animals/cute-baby-animals-10.jpg" alt="cuteanimal">' + '<a href="/">home</a>'
+    )
+  } else if (request.url === '/random-joke') {
+    randInt = parseInt(Math.random())
+    randInt2 = parseInt(Math.random(500))
+    if (randInt > randInt2) {
+      response.end('<h1>Random Joke</h1>' + '<p>Knock, Knock! <br> Who is there?</br> <br> no one this is not a door, but a website</br></p>' + '<a href="/">home</a>')
+    } else if (randInt < randInt2) {
+      response.end('<h1>Random Joke</h1>' + '<p>Knock, Knock! <br> who is there? </br> <br>Lauren, asking Stuart nicely for a 4.0 :) please!</br></p>' + '<a href="/">home</a>'
+        )
+    } else if (randInt === randInt2) {
+      response.end('<h1>Random Joke</h1>' + '<p>Knock, knock! <br> who is there? </br> <br>the end of the semester, finally!</br></p>' + '<a href="/">home</a>')
+    } else {
+      response.end('<h1>Random Joke</h1>' + '<p>Knock, Knock! <br> who is there? </br> <br>hopefully nice weather</br></p>' + '<a href="/">home</a>')
+    }
+  } else {
+    response.end('<h1>Page Not Found</h1>' + '<a href="/">home</a>' + '<p>requested page: ' + request.url + '</p>'
+  )
+  }
 })
 
 // Listen on port 8080, so that we can reach the app at
 // localhost:8080
-var port = 8080
+var port = process.env.PORT || 8080
 server.listen(port)
 
 // Output a friendly message to the terminal
