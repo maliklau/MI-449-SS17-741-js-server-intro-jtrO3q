7f3867546f8aa0d400f05b6e84f749df98efaeb0
maliklau <malikl0799@gmail.com>
2017-04-23 15:16:03 -0400

    fixed random

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 // Require Node's http module and assign it to a variable
 var http = require('http')
-var randInt
 var randInt2
 
 var server = http.createServer(function (request, response) {
@@ -14,14 +13,13 @@ var server = http.createServer(function (request, response) {
       '<img src="http://static.boredpanda.com/blog/wp-content/uuuploads/cute-baby-animals/cute-baby-animals-10.jpg" alt="cuteanimal">' + '<a href="/">home</a>'
     )
   } else if (request.url === '/random-joke') {
-    randInt = parseInt(Math.random())
-    randInt2 = parseInt(Math.random(500))
-    if (randInt > randInt2) {
+    randInt2 = parseInt(Math.random() * 100)
+    if (randInt2 >= 25 && randInt2 < 50) {
       response.end('<h1>Random Joke</h1>' + '<p>Knock, Knock! <br> Who is there?</br> <br> no one this is not a door, but a website</br></p>' + '<a href="/">home</a>')
-    } else if (randInt < randInt2) {
+    } else if (randInt2 >= 0 && randInt2 < 25) {
       response.end('<h1>Random Joke</h1>' + '<p>Knock, Knock! <br> who is there? </br> <br>Lauren, asking Stuart nicely for a 4.0 :) please!</br></p>' + '<a href="/">home</a>'
         )
-    } else if (randInt === randInt2) {
+    } else if (randInt2 >= 50 && randInt2 < 75) {
       response.end('<h1>Random Joke</h1>' + '<p>Knock, knock! <br> who is there? </br> <br>the end of the semester, finally!</br></p>' + '<a href="/">home</a>')
     } else {
       response.end('<h1>Random Joke</h1>' + '<p>Knock, Knock! <br> who is there? </br> <br>hopefully nice weather</br></p>' + '<a href="/">home</a>')
